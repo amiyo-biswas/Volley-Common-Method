fae76244b1afb3e5d43297f06544edbf17864dcf
amiyo <amiyob@coral.in>
2017-06-28 16:10:07 +0530

    Create a project . Name Common_Volley

---
+++ Common_Volley/app/src/main/java/in/intellij_amiya/common_volley/MainActivity.java
@@ -0,0 +1,14 @@
+package in.intellij_amiya.common_volley;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
